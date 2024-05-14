c8ee3ce3d3e85c2f5cf638c66cbebb31c67d5392
이태현 <tedxpem@naver.com>
2024-05-14 16:05:39 +0900

    Add: front, backend init

---
+++ backend/src/main/java/com/ssafy/zimssa/ZimssaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.zimssa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZimssaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZimssaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/zimssa/ZimssaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.zimssa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZimssaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
