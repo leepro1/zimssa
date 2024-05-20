d1a22d0f222aaf2dde683bad27508a7a939770ef
taehyeon <tedxpem@naver.com>
2024-05-21 01:32:37 +0900

    Feat: 게시판 CRUD API 구현
    글 생성, 삭제, 수정 구현

--- backend/src/main/java/com/ssafy/zimssa/board/model/service/BoardServiceImpl.java
+++ backend/src/main/java/com/ssafy/zimssa/board/model/service/BoardServiceImpl.java
@@ -1,9 +1,14 @@
 package com.ssafy.zimssa.board.model.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.zimssa.board.model.BoardDto;
+import com.ssafy.zimssa.board.model.BoardListDto;
 import com.ssafy.zimssa.board.model.mapper.BoardMapper;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -39,4 +44,46 @@ public class BoardServiceImpl implements BoardService {
 		boardMapper.updateHit(articleNo);
 	}
 
+	@Override
+	public BoardListDto listArticle(Map<String, String> map) throws Exception {
+		Map<String, Object> param = new HashMap<String, Object>();
+		param.put("word", map.get("word") == null ? "" : map.get("word"));
+		
+		int currentPage = Integer.parseInt(map.get("pgno") == null ? "1" : map.get("pgno"));
+		int sizePerPage = Integer.parseInt(map.get("spp") == null ? "20" : map.get("spp"));
+		int start = currentPage * sizePerPage - sizePerPage;
+		
+		param.put("start", start);
+		param.put("listsize", sizePerPage);
+
+		String key = map.get("key");
+		param.put("key", key == null ? "" : key);
+		if ("user_id".equals(key))
+			param.put("key", key == null ? "" : "b.user_id");
+		List<BoardDto> list = boardMapper.listArticle(param);
+
+		if ("user_id".equals(key))
+			param.put("key", key == null ? "" : "user_id");
+		int totalArticleCount = boardMapper.getTotalArticleCount(param);
+		int totalPageCount = (totalArticleCount - 1) / sizePerPage + 1;
+
+		BoardListDto boardListDto = new BoardListDto();
+		boardListDto.setArticles(list);
+		boardListDto.setCurrentPage(currentPage);
+		boardListDto.setTotalPageCount(totalPageCount);
+
+		return boardListDto;
+	}
+	
+	@Override
+	public void modifyArticle(BoardDto boardDto) throws Exception {
+		// TODO : BoardDaoImpl의 modifyArticle 호출
+		boardMapper.modifyArticle(boardDto);
+	}
+	
+	@Override
+	public void deleteArticle(int articleNo) throws Exception {
+		// TODO : BoardDaoImpl의 deleteArticle 호출
+		boardMapper.deleteArticle(articleNo);
+	}
 }
